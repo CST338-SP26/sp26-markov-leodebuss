82f2c87dba8c17d70073b8f363b3f8605aaac368
leodebuss <bionerd729@gmail.com>
2026-02-17 11:22:59 -0800

    declaring vars and methods, not implemenation.

---
+++ src/main/java/Markov.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Markov() {
+    // constants
+    private static String BEGINS_SENTENCE = "__$";
+    private static String PUNCTUATION_MARKS = ".!?$";
+
+    // members
+    private HashMap<String, ArrayList<String>> words;
+    private String prevWord;
+
+    // methods
+    public Markov(String filename) {
+
+    }
+    public String getSentence(){
+
+    }
+    void addFromFile(String filename) {
+
+    }
+    void addWord(String prevWord) {
+
+    }
+    String randomWord(String word) {
+
+    }
+    public String toString() {
+
+    }
+
+    public HashMap<String, ArrayList<String>> getWords() {
+        return words;
+    }
+    void addLine(String line) {
+
+    }
+    public static boolean endsWithPunctuation(String senString) {
+
+    }
+}
