f04d2e2f3a224759d945665495bfb2bb0f34a50e
leodebuss <bionerd729@gmail.com>
2026-03-06 11:51:20 -0800

    Comments and abstract done, don't know why it doesn't wanna run tho

--- src/main/java/Markov.java
+++ src/main/java/Markov.java
@@ -18,11 +18,26 @@ public class Markov() {
     private String prevWord;
 
     // methods
+    /**
+     * Constructor for the Markov generator.
+     *
+     * Initializes the HashMap and creates the starting
+     * BEGINS_SENTENCE key.
+     */
     public Markov() {
         words = new HashMap<>();
         words.put(BEGINS_SENTENCE, new ArrayList<>());
         prevWord = BEGINS_SENTENCE;
     }
+
+    /**
+     * Generates a sentence using the Markov chain.
+     *
+     * The sentence starts with BEGINS_SENTENCE and continues selecting
+     * random successor words until a word ending in punctuation is reached.
+     *
+     * @return a generated sentence
+     */
     public String getSentence(){
         StringBuilder sentence = new StringBuilder();
         String word = randomWord(BEGINS_SENTENCE);
@@ -38,6 +53,12 @@ public class Markov() {
         }
         return sentence.toString();
     }
+
+    /**
+     * Reads a file and adds all lines to the Markov structure.
+     *
+     * @param filename the name of the file to read
+     */
     void addFromFile(String filename) {
         try {
             Scanner file = new Scanner(new File(filename));
@@ -51,6 +72,15 @@ public class Markov() {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Adds a single word to the Markov structure.
+     *
+     * The word is stored as a possible successor to the previous word.
+     * If the word ends in punctuation, the next word will start a new sentence.
+     *
+     * @param word the word to add
+     */
     void addWord(String prevWord) { // might need to make public
         if (!words.containsKey(prevWord)) {
             words.put(prevWord, new ArrayList<>());
@@ -65,6 +95,15 @@ public class Markov() {
             prevWord = word;
         }
     }
+
+    /**
+     * Selects a random word that follows the given word.
+     *
+     * If only one word exists in the list, it returns that word directly.
+     *
+     * @param word the word whose successor should be selected
+     * @return a randomly selected following word, or null if none exist
+     */
     String randomWord(String word) { // might need to make public
         ArrayList<String> list = words.get(word);
         if (list == null || list.size() == 0) {
@@ -76,13 +115,30 @@ public class Markov() {
         Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
+
+    /**
+     * Returns a string representation of the Markov word map.
+     *
+     * @return the HashMap as a string
+     */
     public String toString() {
         return words.toString();
     }
 
+    /**
+     * Returns the HashMap containing the Markov word relationships.
+     *
+     * @return the HashMap of words and their possible following words
+     */
     public HashMap<String, ArrayList<String>> getWords() {
         return words;
     }
+
+    /**
+     * Processes a line of text and adds each word to the Markov structure.
+     *
+     * @param line the line of text to process
+     */
     void addLine(String line) { // might need to make public
         Scanner sc = new Scanner(line);
         while (sc.hasNext()) {
@@ -90,6 +146,13 @@ public class Markov() {
         }
         sc.close();
     }
+
+    /**
+     * Determines whether a word ends with a punctuation mark.
+     *
+     * @param word the word to check
+     * @return true if the word ends with punctuation, otherwise false
+     */
     public static boolean endsWithPunctuation(String word) {
         if (word == null || word.length() == 0) {
             return false;
